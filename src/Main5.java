b7b75a35656390775e5ec0f0ecb4858adbba1323
eu-001 <chcolatei71@gmail.com>
2025-10-18 18:54:38 +0900

    10/18

--- src/Main5.java
+++ src/Main5.java
@@ -23,10 +23,24 @@ public class Main5  {
         int sum = 0;
                             // scores.length를 쓰면 배열 크기가 바뀌어도 자동 대응됨.
         for (int i = 0; i < scores.length; i++) {
-            sum += scores[i];
+            sum += scores[i]; //첫 sum 값이 0인데 거깃다 배열 순회를 하면서 각 값을 더하는 것
         }
  
         System.out.println("총합" + sum);
-        System.out.println("평균" + (sum/ scores.length));        
+        System.out.println("평균" + (sum/ scores.length));     
+        
+        add();
+    }
+
+    public static void add() {
+        int[] nums = {3,7,2,9,5};
+        int max = nums [0]; //첫값으로 초기화
+
+        for (int i =1 ; i<nums.length; i++){
+            if (nums[i]> max) {
+                max = nums[i];// nums[i] 가 배열의 첫값보다 크면 그게 max값으로 변환된다는 뜻
+            }
+        }
+        System.out.println("최댓값" + max);
     }
 }
